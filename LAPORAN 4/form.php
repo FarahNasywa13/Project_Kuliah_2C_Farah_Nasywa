fca8b0e0c39ec5808d947e68f0b06fe0945f291c
farahnasywa <farahnasywa.net@gmail.com>
2023-10-03 16:30:38 +0700

    tugass

--- LAPORAN 4/form.php
+++ LAPORAN 4/form.php
@@ -6,26 +6,15 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="proses.php" method="post">
-        <label for="">Nilai1</label>
-        <input type="text" name="nilai1">
-        <br>
-        <label for="">Nilai2</label>
-        <input type="text" name="nilai2">
-        <br>
-        <input type="submit" name="submit" value="Proses">
+    <form method ="form" action ="proses.php">
+        <label> Nilai 1</label>
+        <input type="text" name="nilai1"></br>
+
+        <label> Nilai 2</label>
+        <input type="text" name="nilai2"></br>
+
+        <input type="submit" name="submit" value="proses">
     </form>
+    
 </body>
 </html>
-
-<!-- <?php
-echo $_GET['nilai1']. "<br>";
-echo $_GET['nilai2'];
-?> -->
-
-<?php
-echo $_REQUEST['nilai1']. "<br>". $_GET['nilai2'];
-?>
-
-
-
--- LAPORAN 4/proses.php
+++ LAPORAN 4/proses.php
@@ -1,4 +1,4 @@
 <?php
-echo $_REQUEST['nilai1']. "<br>". $_POST['nilai2'];
-echo $_POST['nilai1']+$_POST['nilai2']; 
+
+echo $_REQUEST['nilai1']+$_GET['nilai2'];
 ?>
