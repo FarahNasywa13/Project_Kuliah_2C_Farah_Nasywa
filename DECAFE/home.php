0d58f54c065a7fc94ce1cbcfc7f0bc990a4efdaf
FarahNasywa13 <farahnasywa.net@gmail.com>
2023-11-14 09:15:10 +0700

    update

--- DECAFE/home.php
+++ DECAFE/home.php
@@ -4,7 +4,27 @@ $query = mysqli_query($conn, "SELECT * FROM tb_daftar_menu");
 while ($row = mysqli_fetch_array($query)) {
     $result[] = $row;
 }
+$query_chart = mysqli_query($conn, "SELECT nama_menu, tb_daftar_menu.id, SUM(tb_list_order.jumlah) AS total_jumlah FROM tb_daftar_menu
+LEFT JOIN tb_list_order ON tb_daftar_menu.id = tb_list_order.menu
+GROUP BY tb_daftar_menu.id
+ORDER BY tb_daftar_menu.id ASC");
+
+//$result_chart = array();
+while ($record_chart = mysqli_fetch_array($query_chart)){
+    $result_chart[] = $record_chart;
+}
+$array_menu = array_column($result_chart, 'nama_menu');
+$array_menu_qoute = array_map(function($menu){
+    return "'".$menu."'";
+}, $array_menu);
+$string_menu = implode(',', $array_menu_qoute);
+//echo $string_menu."<br>";
+
+$array_jumlah_pesanan = array_column($result_chart, 'total_jumlah');
+$string_jumlah_pesanan = implode(',', $array_jumlah_pesanan);
+//echo $string_jumlah_pesanan;
 ?>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <div class="col-lg-9 mt-2">
 
     <!-- Carousel -->
@@ -18,9 +38,7 @@ while ($row = mysqli_fetch_array($query)) {
                 $firstSlideButton = false;
             ?>
 
-                <button type="button" data-bs-target="#carouselExampleCaptions" 
-                data-bs-slide-to="<?php echo $slide ?>" class="<?php echo $aktif ?>" 
-                aria-current="true" aria-label="Slide <?php echo $slide + 1 ?>"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="<?php echo $slide ?>" class="<?php echo $aktif ?>" aria-current="true" aria-label="Slide <?php echo $slide + 1 ?>"></button>
             <?php
                 $slide++;
             } ?>
@@ -34,8 +52,7 @@ while ($row = mysqli_fetch_array($query)) {
                 $firstSlide = false;
             ?>
                 <div class="carousel-item <?php echo $aktif ?>">
-                    <img src="assets/img/<?php echo $data['foto'] ?>" class="img-fluid" 
-                    style="height: 250px; width: 1000px; object-fit: cover" alt="...">
+                    <img src="assets/img/<?php echo $data['foto'] ?>" class="img-fluid" style="height: 250px; width: 1000px; object-fit: cover" alt="...">
                     <div class="carousel-caption d-none d-md-block">
                         <h5><?php echo $data['nama_menu'] ?></h5>
                         <p><?php echo $data['keterangan'] ?></p>
@@ -54,6 +71,7 @@ while ($row = mysqli_fetch_array($query)) {
     </div>
     <!-- Akhir Carousel -->
 
+    <!-- Judul -->
     <div class="card mt-4 border-0 bg-light">
         <div class="card-body text-center">
             <h5 class="card-title">DECAFE - APLIKASI PEMESANAN MAKANAN DAN MINUMAN CAFE</h5>
@@ -61,4 +79,45 @@ while ($row = mysqli_fetch_array($query)) {
             <a href="order" class="btn btn-primary">Buat Order</a>
         </div>
     </div>
+    <!-- Akhir Judul -->
+
+    <!-- Chart -->
+    <div class="card mt-4 border-0 bg-light">
+        <div class="card-body text-center">
+            <div>
+                <canvas id="myChart"></canvas>
+            </div>
+            <script>
+                const ctx = document.getElementById('myChart');
+
+                new Chart(ctx, {
+                    type: 'bar',
+                    data: {
+                        labels: [<?php echo $string_menu ?>],
+                        datasets: [{
+                            label: 'Jumlah Porsi Terjual',
+                            data: [<?php echo $string_jumlah_pesanan ?>],
+                            borderWidth: 1,
+                            backgroundColor:[
+                                'rgba(185, 15, 15, 1)',
+                            'rgba(24, 63, 160, 1)',
+                            'rgba(234, 227, 37, 1)',
+                            'rgba(24, 203, 85, 1)',
+                            'rgba(106, 33, 207, 1)',
+                            'rgba(201, 136, 25, 1)',
+                        ]
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            y: {
+                                beginAtZero: true
+                            }
+                        }
+                    }
+                });
+            </script>
+        </div>
+    </div>
+    <!-- Akhir Chart -->
 </div>
